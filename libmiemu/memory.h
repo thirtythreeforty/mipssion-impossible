bc4c997f4ce08b8ed423c29c8c16da9ebf1a2903
George Hilliard <thirtythreeforty@gmail.com>
2016-03-23 01:33:52 -0500

    Add sample test

--- libmiemu/memory.h
+++ libmiemu/memory.h
@@ -1,8 +1,12 @@
 #ifndef MEMORY_H
 #define MEMORY_H
 
+#include <cstdint>
+
 class Memory
 {
+public:
+	std::size_t size() const { return 512; }
 };
 
 #endif
--- tests/mitests.cpp
+++ tests/mitests.cpp
@@ -1,3 +1,9 @@
 #include "gtest/gtest.h"
 
+#include "memory.h"
 
+TEST(memory, memory_sane)
+{
+	Memory mem;
+	ASSERT_EQ(512, mem.size());
+}
