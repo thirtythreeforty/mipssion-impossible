bccbe36d68228b7efb4587c435287d410bbac0a1
George Hilliard <thirtythreeforty@gmail.com>
2016-04-04 19:03:27 -0500

    Add tests for ID

---
+++ libmiemu/instructions.h
@@ -0,0 +1,72 @@
+#ifndef INSTRUCTIONS_H
+#define INSTRUCTIONS_H
+
+#include <cassert>
+#include <cstdint>
+
+namespace inst
+{
+
+	namespace
+	{
+		constexpr uint16_t rtype(const uint8_t opcode, const uint8_t write_reg, const uint8_t reg1, const uint8_t reg2) {
+			return (opcode << 12) | (write_reg << 8) | (reg1 << 4) | (reg2 << 0);
+		}
+
+		constexpr uint16_t jtype(const uint8_t opcode, const uint8_t address) {
+			return (opcode << 12) | (address >> 1);
+		}
+
+		constexpr uint16_t itype(const uint8_t opcode, const uint8_t write_reg, const uint8_t read_reg, const uint8_t constant) {
+			assert(constant <= 0xF);
+			return (opcode << 12) | (write_reg << 8) | (read_reg << 4) | (constant << 0);
+		}
+	}
+
+constexpr uint16_t addi(const uint8_t write_reg, const uint8_t read_reg, const uint8_t constant)
+{
+	return itype(0x0, write_reg, read_reg, constant);
+}
+
+constexpr uint16_t add(const uint8_t write_reg, const uint8_t reg1, const uint8_t reg2)
+{
+	return rtype(0x1, write_reg, reg1, reg2);
+}
+
+constexpr uint16_t j(const uint16_t address)
+{
+	return jtype(0x9, address);
+}
+
+constexpr uint16_t blt(const uint8_t reg1, const uint8_t reg2, const uint8_t offset)
+{
+	assert(offset <= 0xF);
+	uint8_t extend_off = offset >= 0x8 ? offset | 0xF0 : offset;
+	return itype(0x8, extend_off >> 1, reg2, reg1);
+}
+
+constexpr uint16_t beq(const uint8_t reg1, const uint8_t reg2, const uint8_t offset)
+{
+	assert(offset <= 0xF);
+	uint8_t extend_off = offset >= 0x8 ? offset | 0xF0 : offset;
+	return itype(0xC, extend_off >> 1, reg2, reg1);
+}
+
+constexpr uint16_t lw(const uint8_t write_reg, const uint8_t addr_reg, const uint8_t constant)
+{
+	return itype(0xD, constant, addr_reg, write_reg);
+}
+
+constexpr uint16_t sw(const uint8_t read_reg, const uint8_t addr_reg, const uint8_t constant)
+{
+	return itype(0xE, constant, addr_reg, read_reg);
+}
+
+constexpr uint16_t lbi(const uint8_t reg, const uint8_t constant)
+{
+	return itype(0xF, (constant << 4) & 0xF, constant & 0xF, reg);
+}
+
+}
+
+#endif
---
+++ tests/test_id.cpp
@@ -0,0 +1,151 @@
+#include "gtest/gtest.h"
+
+#include "datapath/ID.h"
+#include "instructions.h"
+
+#include <array>
+
+#define DONTCARE 0
+
+TEST(ID, JumpPCAddress)
+{
+	IFID ifid;
+	ifid.pc_plus_2 = 0xEEEE;
+	ifid.instruction = inst::j(0x0010);
+
+	ID id;
+	id.signals_in(ifid, 0, 0);
+	id.tick();
+	id.tock();
+
+	// New address should be 0xE000 | 0x0010
+	EXPECT_EQ(0xE000 | 0x0010, id.new_pc_address_out());
+}
+
+TEST(ID, RegistersWrite)
+{
+	ID id;
+	IFID ifid;
+	for(int i = 1; i < 16; ++i) {
+		id.signals_in(ifid, i, i);
+		id.tick();
+		id.tock();
+	}
+
+	for(int i = 1; i < 16; ++i) {
+		ifid.instruction = inst::add(0, i, i);
+		id.signals_in(ifid, 0, 0);
+		id.tick();
+		id.tock();
+		ASSERT_EQ(i, id.signals_out().data1);
+		ASSERT_EQ(i, id.signals_out().data2);
+	}
+}
+
+TEST(ID, BranchPCAddress)
+{
+	ID id;
+	IFID ifid;
+	ifid.pc_plus_2 = 0xC0DE;
+
+	id.signals_in(ifid, 1, 1);
+	id.tick();
+	id.tock();
+	id.signals_in(ifid, 2, 2);
+	id.tick();
+	id.tock();
+
+	// This should not "branch"
+	ifid.instruction = inst::blt(2, 1, 4);
+	id.tick();
+	id.tock();
+	EXPECT_EQ(ifid.pc_plus_2, id.new_pc_address_out());
+
+	// This SHOULD "branch"
+	ifid.instruction = inst::blt(1, 2, 4);
+	id.tick();
+	id.tock();
+	EXPECT_EQ(ifid.pc_plus_2 + 4, id.new_pc_address_out());
+
+	// This should not "branch"
+	ifid.instruction = inst::beq(2, 1, 4);
+	id.tick();
+	id.tock();
+	EXPECT_EQ(ifid.pc_plus_2, id.new_pc_address_out());
+
+	// This SHOULD "branch"
+	ifid.instruction = inst::beq(0, 0, 4);
+	id.tick();
+	id.tock();
+	EXPECT_EQ(ifid.pc_plus_2 + 4, id.new_pc_address_out());
+
+	// This branches backward by 2
+	ifid.instruction = inst::beq(0, 0, 0xE);
+	id.tick();
+	id.tock();
+	EXPECT_EQ(ifid.pc_plus_2 - 2, id.new_pc_address_out());
+}
+
+TEST(ID, WriteRegDecode)
+{
+	ID id;
+	IFID ifid;
+	for(int i = 0; i < 16; ++i) {
+		ifid.instruction = inst::addi(i, DONTCARE, DONTCARE);
+		id.signals_in(ifid, 0, 0);
+		id.tick();
+		id.tock();
+		ASSERT_EQ(i, id.signals_out().write_reg);
+	}
+}
+
+TEST(ID, WriteDataSignExtend)
+{
+	ID id;
+	IFID ifid;
+	const std::array<uint16_t (*)(const uint8_t, const uint8_t, const uint8_t), 5> itypes = {
+		inst::addi,
+		inst::blt,
+		inst::beq,
+		inst::lw,
+		inst::sw,
+		// Test lbi separately
+	};
+
+	for(const auto& itype: itypes) {
+		for(int i = 0; i < 0x8; ++i) {
+			ifid.instruction = itype(DONTCARE, DONTCARE, i);
+			id.signals_in(ifid, 0, 0);
+			id.tick();
+			id.tock();
+			ASSERT_EQ(i, id.signals_out().write_data);
+		}
+
+		// Must sign-extend if constant is 0x8 thru 0xF
+		for(int i = 0x8; i < 0xF; ++i) {
+			ifid.instruction = inst::addi(DONTCARE, DONTCARE, i);
+			id.signals_in(ifid, 0, 0);
+			id.tick();
+			id.tock();
+			ASSERT_EQ(-i, id.signals_out().write_data);
+		}
+	}
+}
+
+TEST(ID, WriteDataLBI)
+{
+	ID id;
+	IFID ifid;
+
+	// Should sign-extend negative
+	ifid.instruction = inst::lbi(DONTCARE, 0xFF);
+	id.tick();
+	id.tock();
+	EXPECT_EQ(0xFFFF, id.signals_out().write_data);
+
+	// Should sign-extend positive
+	ifid.instruction = inst::lbi(DONTCARE, 0x7F);
+	id.tick();
+	id.tock();
+	EXPECT_EQ(0x007F, id.signals_out().write_data);
+}
