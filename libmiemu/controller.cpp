ed4bf03eae54b95a18fa9dd4271d72031dd09a1a
Ashley E. Stallings <aes421@msstate.edu>
2016-04-08 23:43:17 -0500

    Control Signal Implementation

--- libmiemu/controller.cpp
+++ libmiemu/controller.cpp
@@ -2,9 +2,378 @@
 
 void Controller::signals_in(uint16_t instruction)
 {
+	_instruction = instruction;
+	_opcode = _instruction & 0xF000;
 }
 
 Controls Controller::controls_out() const
 {
-	return {};
+	Controls all_controls;
+	IFControls if_controls;
+	IDControls id_controls;
+	EXControls ex_controls;
+	MEMControls mem_controls;
+	WBControls wb_controls;
+
+	switch (_opcode) {
+	case ADDI:
+		//IF
+		if_controls.use_new_address = 0;
+
+		//ID
+		id_controls.branch_lt = 0;
+		id_controls.branch_z = 0;
+		id_controls.jump = 0;
+		id_controls.reg_dst = 1;
+		id_controls.reg_write = 1;
+
+		//EX
+		ex_controls.alu_op = ALUOp::Add;
+		ex_controls.alu_src = 1;
+
+		//MEM
+		mem_controls.mem_write = 0;
+
+		//WB
+		wb_controls.mem_to_reg = 0;
+		break;
+
+	case ADD:
+		//IF
+		if_controls.use_new_address = 0;
+
+		//ID
+		id_controls.branch_lt = 0;
+		id_controls.branch_z = 0;
+		id_controls.jump = 0;
+		id_controls.reg_dst = 1;
+		id_controls.reg_write = 1;
+
+		//EX
+		ex_controls.alu_op = ALUOp::Add;
+		ex_controls.alu_src = 0;
+
+		//MEM
+		mem_controls.mem_write = 0;
+
+		//WB
+		wb_controls.mem_to_reg = 0;
+		break;
+
+	case SUB:
+		//IF
+		if_controls.use_new_address = 0;
+
+		//ID
+		id_controls.branch_lt = 0;
+		id_controls.branch_z = 0;
+		id_controls.jump = 0;
+		id_controls.reg_dst = 1;
+		id_controls.reg_write = 1;
+
+		//EX
+		ex_controls.alu_op = ALUOp::Subtract;
+		ex_controls.alu_src = 0;
+
+		//MEM
+		mem_controls.mem_write = 0;
+
+		//WB
+		wb_controls.mem_to_reg = 0;
+		break;
+
+	case AND:
+		//IF
+		if_controls.use_new_address = 0;
+
+		//ID
+		id_controls.branch_lt = 0;
+		id_controls.branch_z = 0;
+		id_controls.jump = 0;
+		id_controls.reg_dst = 1;
+		id_controls.reg_write = 1;
+
+		//EX
+		ex_controls.alu_op = ALUOp::And;
+		ex_controls.alu_src = 0;
+
+		//MEM
+		mem_controls.mem_write = 0;
+
+		//WB
+		wb_controls.mem_to_reg = 0;
+		break;
+
+	case OR:
+		//IF
+		if_controls.use_new_address = 0;
+
+		//ID
+		id_controls.branch_lt = 0;
+		id_controls.branch_z = 0;
+		id_controls.jump = 0;
+		id_controls.reg_dst = 1;
+		id_controls.reg_write = 1;
+
+		//EX
+		ex_controls.alu_op = ALUOp::Or;
+		ex_controls.alu_src = 0;
+
+		//MEM
+		mem_controls.mem_write = 0;
+
+		//WB
+		wb_controls.mem_to_reg = 0;
+		break;
+
+	case XOR:
+		//IF
+		if_controls.use_new_address = 0;
+
+		//ID
+		id_controls.branch_lt = 0;
+		id_controls.branch_z = 0;
+		id_controls.jump = 0;
+		id_controls.reg_dst = 1;
+		id_controls.reg_write = 1;
+
+		//EX
+		ex_controls.alu_op = ALUOp::Xor;
+		ex_controls.alu_src = 0;
+
+		//MEM
+		mem_controls.mem_write = 0;
+
+		//WB
+		wb_controls.mem_to_reg = 0;
+		break;
+
+	case SLL:
+		//IF
+		if_controls.use_new_address = 0;
+
+		//ID
+		id_controls.branch_lt = 0;
+		id_controls.branch_z = 0;
+		id_controls.jump = 0;
+		id_controls.reg_dst = 1;
+		id_controls.reg_write = 1;
+
+		//EX
+		ex_controls.alu_op = ALUOp::ShiftLeft;
+		ex_controls.alu_src = 1;
+
+		//MEM
+		mem_controls.mem_write = 0;
+
+		//WB
+		wb_controls.mem_to_reg = 0;
+		break;
+
+	case SRL:
+		//IF
+		if_controls.use_new_address = 0;
+
+		//ID
+		id_controls.branch_lt = 0;
+		id_controls.branch_z = 0;
+		id_controls.jump = 0;
+		id_controls.reg_dst = 1;
+		id_controls.reg_write = 1;
+
+		//EX
+		ex_controls.alu_op = ALUOp::ShiftRight;
+		ex_controls.alu_src = 1;
+
+		//MEM
+		mem_controls.mem_write = 0;
+
+		//WB
+		wb_controls.mem_to_reg = 0;
+		break;
+
+	case BLT:
+		//IF
+		if_controls.use_new_address = 1;
+
+		//ID
+		id_controls.branch_lt = 1;
+		id_controls.branch_z = 0;
+		id_controls.jump = 0;
+		id_controls.reg_dst = DONT_CARE;
+		id_controls.reg_write = 0;
+
+		//EX
+		ex_controls.alu_op = ALUOp::Subtract;
+		ex_controls.alu_src = 0;
+
+		//MEM
+		mem_controls.mem_write = 0;
+
+		//WB
+		wb_controls.mem_to_reg = 0;
+		break;
+
+	case J:
+		//IF
+		if_controls.use_new_address = 1;
+
+		//ID
+		id_controls.branch_lt = 0;
+		id_controls.branch_z = 0;
+		id_controls.jump = 1;
+		id_controls.reg_dst = DONT_CARE;
+		id_controls.reg_write = 0;
+
+		//EX
+		//ex_controls.alu_op = 1001;
+		ex_controls.alu_src = 0;
+
+		//MEM
+		mem_controls.mem_write = 0;
+
+		//WB
+		wb_controls.mem_to_reg = 0;
+		break;
+
+	case JL:
+		//IF
+		if_controls.use_new_address = 1;
+
+		//ID
+		id_controls.branch_lt = 0;
+		id_controls.branch_z = 0;
+		id_controls.jump = 1;
+		id_controls.reg_dst = DONT_CARE;
+		id_controls.reg_write = 0;
+
+		//EX
+		//ex_controls.alu_op = 1010;
+		ex_controls.alu_src = 0;
+
+		//MEM
+		mem_controls.mem_write = 0;
+
+		//WB
+		wb_controls.mem_to_reg = 0;
+		break;
+
+	case JR:
+		//IF
+		if_controls.use_new_address = 1;
+
+		//ID
+		id_controls.branch_lt = 0;
+		id_controls.branch_z = 0;
+		id_controls.jump = 1;
+		id_controls.reg_dst = DONT_CARE;
+		id_controls.reg_write = 0;
+
+		//EX
+		//ex_controls.alu_op = 1011;
+		ex_controls.alu_src = 0;
+
+		//MEM
+		mem_controls.mem_write = 0;
+
+		//WB
+		wb_controls.mem_to_reg = 0;
+		break;
+
+	case BEQ:
+		//IF
+		if_controls.use_new_address = 1;
+
+		//ID
+		id_controls.branch_lt = 0;
+		id_controls.branch_z = 1;
+		id_controls.jump = 0;
+		id_controls.reg_dst = DONT_CARE;
+		id_controls.reg_write = 0;
+
+		//EX
+		ex_controls.alu_op = ALUOp::Subtract;
+		ex_controls.alu_src = 0;
+
+		//MEM
+		mem_controls.mem_write = 0;
+
+		//WB
+		wb_controls.mem_to_reg = 0;
+		break;
+
+	case LW:
+		//IF
+		if_controls.use_new_address = 0;
+
+		//ID
+		id_controls.branch_lt = 0;
+		id_controls.branch_z = 0;
+		id_controls.jump = 0;
+		id_controls.reg_dst = 0;
+		id_controls.reg_write = 1;
+
+		//EX
+		ex_controls.alu_op = ALUOp::Add;
+		ex_controls.alu_src = 0;
+
+		//MEM
+		mem_controls.mem_write = 0;
+
+		//WB
+		wb_controls.mem_to_reg = 1;
+		break;
+
+	case SW:
+		//IF
+		if_controls.use_new_address = 0;
+
+		//ID
+		id_controls.branch_lt = 0;
+		id_controls.branch_z = 0;
+		id_controls.jump = 0;
+		id_controls.reg_dst = DONT_CARE;
+		id_controls.reg_write = 0;
+
+		//EX
+		ex_controls.alu_op = ALUOp::Add;
+		ex_controls.alu_src = 0;
+
+		//MEM
+		mem_controls.mem_write = 1;
+
+		//WB
+		wb_controls.mem_to_reg = 0;
+		break;
+
+	case LBI:
+		//IF
+		if_controls.use_new_address = 0;
+
+		//ID
+		id_controls.branch_lt = 0;
+		id_controls.branch_z = 0;
+		id_controls.jump = 0;
+		id_controls.reg_dst = 0;
+		id_controls.reg_write = 1;
+
+		//EX
+		ex_controls.alu_op = ALUOp::Add;
+		ex_controls.alu_src = 1;
+
+		//MEM
+		mem_controls.mem_write = 0;
+
+		//WB
+		wb_controls.mem_to_reg = 1;
+		break;
+	}
+
+	all_controls.id_controls = id_controls;
+	all_controls.if_controls = if_controls;
+	all_controls.ex_controls = ex_controls;
+	all_controls.mem_controls = mem_controls;
+	all_controls.wb_controls = wb_controls;
+
+	return all_controls;
 }
--- libmiemu/controller.h
+++ libmiemu/controller.h
@@ -3,9 +3,29 @@
 
 #include "datapath/signals.h"
 
+#define ADDI 0000
+#define ADD 0001
+#define SUB 0010
+#define AND 0011
+#define OR 0100
+#define XOR 0101
+#define SLL 0110
+#define SRL 0111
+#define BLT 1000
+#define J 1001
+#define JL 1010
+#define JR 1011
+#define BEQ 1100
+#define LW 1101
+#define SW 1110
+#define LBI 1111
+
+#define DONT_CARE 0
+
 class Controller
 {
 	uint16_t _instruction;
+	uint8_t _opcode;
 
 public:
 	void signals_in(uint16_t instruction);
