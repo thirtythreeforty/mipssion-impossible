42eaa093753a1abe75080f9fcba926bf1221d7f3
George Hilliard <thirtythreeforty@gmail.com>
2016-04-24 13:05:28 -0500

    "Fix" JR test
    Still need to work out why hazard detection doesn't insert a NOP for us.

--- tests/test_emulator.cpp
+++ tests/test_emulator.cpp
@@ -224,12 +224,13 @@ TEST(Emulator, jr)
 {
 	Emulator emulator;
 
-	std::array<uint16_t, 8> instructions = {
+	std::array<uint16_t, 9> instructions = {
 		inst::nop(),
 		inst::jl(8),
 		inst::lbi(reg::a0, 2),
-		inst::j(14),
+		inst::j(16),
 		inst::nop(), // Clear data hazard before jr
+		inst::nop(),
 		inst::nop(),
 		inst::jr(),
 		inst::nop()
@@ -239,7 +240,7 @@ TEST(Emulator, jr)
 	step_emulator_to(emulator, finish);
 
 	EXPECT_EQ(4, emulator.get_datapath().get_ID().get_register_file().get_register(14));
-	EXPECT_EQ(0, emulator.get_datapath().get_ID().get_register_file().get_register(reg::a0));
+	EXPECT_EQ(2, emulator.get_datapath().get_ID().get_register_file().get_register(reg::a0));
 }
 
 TEST(Emulator, beq)
